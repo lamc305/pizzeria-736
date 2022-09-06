d01a3ccfc31bc55ac6e0c38bd37b574c7e306838
Luis Medina <lamc3005@gmail.com>
2022-09-06 11:47:30 -0400

    added more actions and function to reducer

--- src/reducer/appReducer.js
+++ src/reducer/appReducer.js
@@ -1,10 +1,12 @@
 import { TYPES } from "../action/actionReducer"
 
 export const initialStateApp = {
-  pizzas: null,
-  empanadas: null,
-  postres: null,
-  bebidas: null,
+  products: {
+    pizzas: null,
+    empanadas: null,
+    postres: null,
+    bebidas: null,
+  },
   cart: []
 }
 
@@ -13,11 +15,33 @@ export const appReducer = (state, action) => {
     case TYPES.CALL_API:
       return {
         ...state,
-        pizzas: action.payload[0],
-        empanadas: action.payload[1],
-        postres: action.payload[2],
-        bebidas: action.payload[3]
+        products: {
+          pizzas: action.payload[0],
+          empanadas: action.payload[1],
+          postres: action.payload[2],
+          bebidas: action.payload[3]
+        }
       }
+    case TYPES.ADD_TO_CART: {
+      let newItem = state.products.empanadas.find(
+        (product) => product.id === action.payload
+      );
+      let itemInCart = state.cart.find((item) => item.id === newItem.id);
+
+      return itemInCart
+        ? {
+          ...state,
+          cart: state.cart.map((item) =>
+            item.id === newItem.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        }
+        : {
+          ...state,
+          cart: [...state.cart, { ...newItem, quantity: 1 }],
+        };
+    }
     default:
       return state
   }
