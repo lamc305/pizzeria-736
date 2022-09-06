f1729a351b64834e869529b1f078e6bbc48075bd
Andres Pulupa <yucoec@gmail.com>
2022-09-06 11:41:37 -0500

    saved changes home

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.jpg";
 import Pizza from "../../assets/Pizza.png";
 import "./home.css";
@@ -18,21 +19,19 @@ function Home() {
             Lorem ipsum dolor sit amet. Sed veritatis rerum et illo porro quo
             ipsa optio aut reiciendis modi rem voluptatibus rerum. Id laborum
             voluptas ab nobis odit sit amet ullam quo voluptates architecto ad
-            iure nostrum. Qui minima ratione quo natus beatae cum similique
-            molestias ea internos deserunt et voluptatem impedit ut quia quasi
-            est provident possimus. Et temporibus ullam et nisi ducimus et
-            reprehenderit nostrum in galisum sint et eveniet provident ut labore
-            quasi et sunt voluptate
+            iure nostrum.
           </p>
         </div>
         <div className="container__toMenu">
           <h3 className="container__title2">Don Remolo</h3>
           <p className="container__paragraph2">La mejor pizza de colegiales!</p>
-          <button className="container__btn" type="button">
-            Hace tu pedido
-          </button>
+          <button className="container__btn">Hace tu pedido</button>
         </div>
       </section>
+
+      <div className="joinchat">
+        <div className="joinchat__button"></div>
+      </div>
     </>
   );
 }
