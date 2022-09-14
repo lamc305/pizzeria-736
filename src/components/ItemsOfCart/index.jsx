84799cdbcbb2f4db7d5f9d4cfaf26c8e49356f80
Luis Medina <lamc3005@gmail.com>
2022-09-14 17:56:14 -0400

    changed styles and animations

--- src/components/ItemsOfCart/index.jsx
+++ src/components/ItemsOfCart/index.jsx
@@ -1,33 +1,30 @@
-import './itemsOfCart.css'
-import { HiMinus, HiOutlineTrash, HiPlus } from 'react-icons/hi'
+import './carrito.css'
+import { HiMinus, HiOutlineTrash, HiPlus } from "react-icons/hi"
 
 function ItemsOfCart({ name, price, description, img }) {
-
   return (
-    <div className='itemsOfCart__product' >
-      <div className='itemsOfCart__image' >
+    <section className='carrito'>
+      <div className='carrito__image'>
         <img src={img} loading='lazy' alt="product" />
       </div>
-      <div className='itemsOfCart__text'>
-        <div>
-          <h2>{name}</h2>
-          <p>{description}</p>
-        </div>
-        <div className='items__buttonsCart'>
-          <div className='items___buttonsLeft'>
-            <button className='item__buttons--left'><HiMinus /></button>
-            <button className='item__buttons--center'>0</button>
-            <button className='item__buttons--left'><HiPlus /></button>
-          </div>
+      <div className="carrito__text">
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="carrito__Buttons">
+        <div className='items__buttonsLeft'>
+          <button className=''><HiMinus /></button>
+          <button className=''>0</button>
+          <button className=''><HiPlus /></button>
         </div>
       </div>
-      <div className='itemsOfCart__price'>
+      <div className='carrito__priceTrash'>
         <h2>${price}</h2>
-        <div className='itemsOfCart__iconTrash'>
+        <button>
           <HiOutlineTrash />
-        </div>
+        </button>
       </div>
-    </div>
+    </section >
   )
 }
 
