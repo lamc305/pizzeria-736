6755fc31be2f43eda299a3cb121ce8a0c247bb75
Luis Medina <lamc3005@gmail.com>
2022-09-20 14:39:06 -0400

    fix bug with home

--- src/components/CartComponents/FormEnvio/index.jsx
+++ src/components/CartComponents/FormEnvio/index.jsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { ModalContexto } from "../../../contexto/modalContext";
-import { ReducerContext } from "../../../contexto/reducerContext";
 import "./form.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
