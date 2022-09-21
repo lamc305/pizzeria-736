2c754a3fa2ee702bf0126a89dbddb55974fbc9e9
Luis Medina <lamc3005@gmail.com>
2022-09-21 18:23:05 -0400

    added modalContext

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { ReducerContextProvider } from "./contexto/reducerContext";
 import AnimatedRoutes from "./components/AnimatedRoutes/AnimatedRoutes";
+import ModalContextProvider from "./contexto/modalContext";
 
 function App() {
   return (
     <BrowserRouter>
       <ReducerContextProvider>
-        <AnimatedRoutes />
+        <ModalContextProvider>
+          <AnimatedRoutes />
+        </ModalContextProvider>
       </ReducerContextProvider>
     </BrowserRouter>
   );
