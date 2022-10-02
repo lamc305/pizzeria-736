5b75939cad526b352445a6663dc79787536de45e
Luis Medina <lamc3005@gmail.com>
2022-10-01 20:08:17 -0400

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ReducerContextProvider } from "./contexto/reducerContext";
 import AnimatedRoutes from "./components/AnimatedRoutes/AnimatedRoutes";
 import ModalContextProvider from "./contexto/modalContext";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ReducerContextProvider>
         <ModalContextProvider>
           <AnimatedRoutes />
         </ModalContextProvider>
       </ReducerContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
